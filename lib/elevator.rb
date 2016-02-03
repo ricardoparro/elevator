f95f207297aff9d6caa517caf842209d07cf569a
Ricardo Parro <ricardo.parro@gmail.com>
2016-02-03 23:22:16 +0100

    continue working on the elevator class

--- lib/elevator.rb
+++ lib/elevator.rb
@@ -2,18 +2,42 @@ class Elevator
 
     attr_accessor :current_direction, :current_floor, :floor_list
     def initialize
-        @current_floor =t0
+        @current_floor = 0
         @floor_list=[]
         @current_direction = Direction::STANDBY
     end
-    
+
+    def start_elevator_engine
+      t1 = Thread.new(process_requests())
+      t2 = Thread.new(receive_requests())
+    end
+
+    def process_requests
+      loop do
+      end
+    end
+
+    def receive_requests
+    end
+
     def change_direction(to_direction)
         @current_direction = to_direction
     end
     
-    def change_floor(floor_number)
-        @current_floor = floor_number
+    def request_floor(floor_number)
         @floor_list.push(floor_number)
+    end
+    def move_to_floor(floor_number)
+
+      if(floor_number > @current_floor)
+        @current_direction = Direction::UP
+      else
+        @current_direction = Direction::DOWN
+      end
+      puts 'moving.....'
+      #sleep(10)
+      @current_floor = floor_number
+      puts 'arrived to floor #{floor_number}'
     end
 
 end
--- tests/test_elevator.rb
+++ tests/test_elevator.rb
@@ -1,11 +1,13 @@
 require 'minitest/spec'
 require 'minitest/autorun'
 require './lib/elevator.rb'
+
 describe Elevator do
 
     before do
          @elevator = Elevator.new 
     end
+    
 
     it "can change direction to up" do
         @elevator.change_direction(Direction::UP)
@@ -17,18 +19,25 @@ describe Elevator do
         assert_equal @elevator.current_direction, Direction::DOWN 
     end
     it "can move to that floor" do
-        @elevator.change_floor(2)
+        @elevator.move_to_floor(2)
         assert_equal(@elevator.current_floor,2)
     end
     it "can maintain a list of floor numbers" do
-        @elevator.change_floor(2);
-        @elevator.change_floor(3);
-        @elevator.change_floor(4);
-        
+        @elevator.request_floor(2);
+        @elevator.request_floor(3);
+        @elevator.request_floor(4); 
+        @elevator.request_floor(1);
+
         assert(@elevator.floor_list.count,3)
-            
+        assert_equal(@elevator.floor_list[0], 2)
+        assert_equal(@elevator.floor_list[1], 3)
+        assert_equal(@elevator.floor_list[2], 4)    
+        assert_equal(@elevator.floor_list[3], 1)    
     end
-    it "can move through the floors"
+
+    it "can move through the floors" do
+        @elevator.move_to_floor(2);
+    end
     it "can reverse direction when reaches the top"
     
 end
